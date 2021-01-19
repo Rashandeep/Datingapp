c3f2fd8b8b0f29ed2af1f10aebf78b01c2cfc072
Rashandeep <rashandeepsingh@gmail.com>
2021-01-19 16:27:19 +0530

    end of section 9

--- datingapp-SPA/src/app/app.module.ts
+++ datingapp-SPA/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,10 +14,23 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { AlertifyService } from './_services/alertify.service';
+import { UserService } from './_services/user.service';
+import { AuthGuard } from './_gaurds/auth.guard';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+
+export function tokenGetter(){
+  return localStorage.getItem('token');
+}
+
 
 
 
@@ -28,7 +43,9 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
   imports: [
     BrowserModule,
@@ -36,11 +53,25 @@ import { appRoutes } from './routes';
     FormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:5000'],
+        disallowedRoutes: ['localhosr:5000/api/auth'],
+      }
+    })
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    MemberDetailResolver,
+    MemberListResolver,
+    AlertifyService,
+    AuthService,
+    UserService,
+    AuthGuard,
   ],
   bootstrap: [AppComponent]
 })
